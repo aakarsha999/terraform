2af3efeb2b5f7ef80475d6b3189e3a13e02b2cb9
aakarsha999 <147119002+aakarsha999@users.noreply.github.com>
2024-05-30 16:54:50 +0530

    main.tf

---
+++ main.tf
@@ -0,0 +1,11 @@
+# main.tf
+
+provider "aws" {
+  region = "us-west-2"
+}
+
+resource "aws_s3_bucket" "example" {
+  bucket = "example-bucket-${random_string.random_id.id}"
+}
+
+[root@ip-20-0-0-55 terraform_file]#
